9315104113d27592b0b5922b3803a05f7f2b890e
jhLee <77824683+nryn6563org@users.noreply.github.com>
2026-01-29 14:34:48 +0900

    Updates

---
+++ nuxt-project/static/1769664884/clients/consultation-notes/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/clients/consultation-notes", {data:[{}],fetch:{},mutations:void 0});
---
+++ nuxt-project/static/1769664884/market/breaking-news/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/market/breaking-news", {data:[{}],fetch:{},mutations:void 0});
